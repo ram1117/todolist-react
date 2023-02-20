4fc839dc7eceb04706cc4437ba653c044403d25e
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-20 19:59:40 +0530

    Add edit task functionality, saving and retrieving data to/from  local storage

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -5,27 +5,36 @@ const InputTodo = ({ addTodoItem }) => {
   InputTodo.defaultProps = { addTodoItem: null };
   InputTodo.propTypes = { addTodoItem: PropTypes.func };
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodoItem(title);
-    setTitle('');
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('Please add todo item:');
+    }
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="input-todo"
-          placeholder="Enter Task.."
-          value={title}
-          onChange={handleChange}
-        />
-        <button type="submit">Add task</button>
-      </form>
+      <div className="inputTodo-wrapper">
+        <form onSubmit={handleSubmit} className="inputTodo-content">
+          <input
+            className="input-todo"
+            placeholder="Enter Task.."
+            value={title}
+            onChange={handleChange}
+          />
+          <button type="submit">Add task</button>
+        </form>
+      </div>
+      <span>{message}</span>
     </>
   );
 };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,31 +1,78 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import styles from '../styles/TodoItem.module.css';
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+const TodoItem = ({
+  itemProp, handleChange, delTodo, updateTask,
+}) => {
   TodoItem.defaultProps = {
     itemProp: null,
     handleChange: null,
     delTodo: null,
+    updateTask: null,
   };
   TodoItem.propTypes = {
     itemProp: PropTypes.objectOf(PropTypes.string),
     handleChange: PropTypes.func,
     delTodo: PropTypes.func,
+    updateTask: PropTypes.func,
+  };
+
+  const [editing, setEditing] = useState(false);
+
+  const viewMode = {};
+  const editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (e) => {
+    if (e.key === 'Enter') {
+      setEditing(false);
+    }
   };
 
   return (
-    <li className="todo-item">
+    <li className={styles.todoItem}>
+      <div className={styles['todoItem-content']} style={viewMode}>
+        <input
+          className={styles.checkBox}
+          type="checkbox"
+          onChange={() => handleChange(itemProp.id)}
+          checked={itemProp.completed}
+        />
+        <p>
+          {itemProp.task}
+        </p>
+        <button
+          type="button"
+          className={styles.todoButton}
+          onClick={handleEditing}
+        >
+          Edit
+        </button>
+        <button
+          className={styles.todoButton}
+          type="button"
+          onClick={() => delTodo(itemProp.id)}
+        >
+          Delete
+        </button>
+      </div>
       <input
-        type="checkbox"
-        onChange={() => handleChange(itemProp.id)}
-        checked={itemProp.completed}
+        className={styles.todoEditText}
+        type="text"
+        defaultValue={itemProp.task}
+        style={editMode}
+        onChange={(e) => { updateTask(e.target.value, itemProp.id); }}
+        onKeyDown={handleUpdatedDone}
       />
-      <input type="text" value={itemProp.task} />
-      <button
-        type="button"
-        onClick={() => delTodo(itemProp.id)}
-      >
-        Delete
-      </button>
     </li>
   );
 };
